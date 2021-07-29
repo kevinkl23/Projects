6d10f8c46edd063bedc04657ad97f73e064c00a4
kevinkl23 <lo.yohaneskevin@gmail.com>
2021-07-29 14:46:55 +0700

    Added functions(init, repr, greetings)

--- WWTBEAM.py
+++ WWTBEAM.py
@@ -1,1 +1,17 @@
 # WHO WANTS TO BE A MILLIONAIRE #
+
+class Main:
+    def __init__(self, userName):
+        self.userName = userName
+
+    def __repr__(self):
+        return self.userName
+
+    def greetings(self):
+        print("Welcome To The Game " + self.userName + "!")
+
+
+name = input("Enter User Name:")
+
+User1 = Main(name)
+User1.greetings()
